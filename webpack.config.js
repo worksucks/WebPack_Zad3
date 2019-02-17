4dfd947811e46435b289c1a2563f8b725bbcb9f6
Tomasz Reizer <t_reizer@yahoo.com>
2019-02-17 14:54:42 +0100

    added files

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+//webpack.config.js
+module.exports = {
+    //mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
